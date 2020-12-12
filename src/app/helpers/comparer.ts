3055b6ea854c3f11da4ed70bc839aa27e0337165
Bruno Jacobs Schulz <bjacobss@everis.com>
2020-12-11 20:13:16 -0500

    added funcionality to the comparer

--- src/app/helpers/comparer.ts
+++ src/app/helpers/comparer.ts
@@ -1,7 +1,10 @@
 import Restaurant from '../types/restaurant';
 
 const isEqual = (a: Restaurant, b: Restaurant) => {
-  return a.name === b.name && a.city === b.city;
+  return (
+    a.name.toLowerCase() === b.name.toLowerCase() &&
+    a.city.toLowerCase() === b.city.toLowerCase()
+  );
 };
 
 export default isEqual;
